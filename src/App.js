996e816b65a9dc1bd8fc03a4b52aec90b6c7d5b8
CodyKat <jaemjeon@student.42seoul.kr>
2023-12-26 19:21:40 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useCallback } from "react";
+import './App.css'
+import Lists from "./components/Lists";
+import Form from "./components/Form";
 
-function App() {
+const initialTodoData = localStorage.getItem('todoData') ? JSON.parse(localStorage.getItem('todoData')) : [];
+
+
+export default function App() {
+
+  const [todoData, setTodoData] = useState(initialTodoData);
+  const [value, setValue] = useState("");
+
+
+  const handleClick = useCallback((id) => {
+    let newTodoData = todoData.filter(data => data.id !== id);
+    setTodoData(newTodoData);
+    localStorage.setItem('todoData', JSON.stringify(newTodoData));
+  }, [todoData]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false
+    };
+
+    setTodoData(prev => [...prev, newTodo]);
+    localStorage.setItem('todoData', JSON.stringify([...todoData, newTodo]));
+    setValue("")
+
+  }
+
+  const handleRemoveClick = () => {
+    setTodoData([]);
+    localStorage.setItem('todoData', JSON.stringify([]));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-100">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
+          <h1>할 일 목록</h1>
+          <button onClick={handleRemoveClick}>Delete All</button>
+        </div>
 
-export default App;
+
+        <Lists todoData={todoData} setTodoData={setTodoData} handleClick={handleClick} />
+        <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
+
+
+      </div>
+    </div>
+  )
+}
